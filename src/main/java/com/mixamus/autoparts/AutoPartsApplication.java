884a66a20cc1aaacd9accc4915d4dae8709bd39e
Maxim Dokuchaev <mixamus@icloud.com>
2021-02-12 15:25:51 +0300

    Initial commit

---
+++ src/main/java/com/mixamus/autoparts/AutoPartsApplication.java
@@ -0,0 +1,13 @@
+package com.mixamus.autoparts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoPartsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoPartsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mixamus/autoparts/AutoPartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mixamus.autoparts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoPartsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
