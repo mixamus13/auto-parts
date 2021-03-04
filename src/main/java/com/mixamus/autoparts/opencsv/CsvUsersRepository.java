63e1e4e644034a4ba19f5f6658a4a98a06e091e6
Maxim Dokuchaev <MDokuchaev@luxoft.com>
2021-03-04 21:11:34 +0300

    add sneaky

--- src/main/java/com/mixamus/autoparts/opencsv/CsvUsersRepository.java
+++ src/main/java/com/mixamus/autoparts/opencsv/CsvUsersRepository.java
@@ -1,10 +1,10 @@
 package com.mixamus.autoparts.opencsv;
 
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -12,16 +12,14 @@ import java.util.List;
 @Component
 public class CsvUsersRepository {
 
+    @SneakyThrows
     public List<String> getPartCsvId(int id) {
         List<List<String>> records = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader("MOCK_DATA_USERS.csv"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] values = line.split(",");
-                records.add(Arrays.asList(values));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        BufferedReader br = new BufferedReader(new FileReader("MOCK_DATA_USERS.csv"));
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] values = line.split(",");
+            records.add(Arrays.asList(values));
         }
         return records.get(id);
     }
