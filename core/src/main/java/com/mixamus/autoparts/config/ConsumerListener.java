88197e6b2cab67346377d9b388fae18cbb06967d
Maxim Dokuchaev <MDokuchaev@luxoft.com>
2021-04-11 15:41:33 +0300

    added-kafka-listener-two

--- core/src/main/java/com/mixamus/autoparts/config/ConsumerListener.java
+++ core/src/main/java/com/mixamus/autoparts/config/ConsumerListener.java
@@ -10,8 +10,13 @@ import static com.mixamus.autoparts.controllers.UserResource.TOPIC_NAME;
 @Slf4j
 public class ConsumerListener {
 
-    @KafkaListener(topics = TOPIC_NAME, groupId = "kafka_example")
-    public void listen(String message) {
-        log.info("This is my message from KafkaListener ---> " + message);
+    @KafkaListener(topics = TOPIC_NAME, groupId = "kafka_example_one")
+    public void listenForOrder(String message) {
+        log.info("This is my message from KafkaListener ---1---> " + message);
+    }
+
+    @KafkaListener(topics = TOPIC_NAME, groupId = "kafka_example_two")
+    public void listenForClient(String message) {
+        log.info("This is my message from KafkaListener ---2---> " + message);
     }
 }
